0454f032bbd87036312e973b2ff0bda136a34fda
Marlon Vargas <mardrex88@gmail.com>
2022-05-25 21:00:23 -0500

    Solucion kata 9

--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -25,7 +25,6 @@ public class Kata7 {
                 .collect(Collectors.toList());
 
 
-        return newList
-                ;
+        return newList;
     }
 }
--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -10,6 +10,7 @@ import util.DataUtil;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -19,7 +20,11 @@ import java.util.Map;
 public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
+        List<Map> newList = movieLists.stream().map((MovieList::getVideos))
+                .flatMap(c->c.stream())
+                .map(movie -> ImmutableMap.of("id", movie.getId(), "title",movie.getTitle(), "time", movie.getInterestingMoments().stream().filter(a -> a.getType().equals("middle")), "url", movie.getBoxarts().stream().reduce((x,y)->x.getWidth()<y.getWidth()?x:y)))
+                .collect(Collectors.toList());
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        return newList;
     }
 }
